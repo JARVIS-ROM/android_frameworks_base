fbfb4778c53a86fe31f70325e6cc16fead3b2913
vjspranav <pranavasri@live.in>
2022-12-16 19:16:15 +0000

    Make expanded volume panel work nice with left panel

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -478,10 +478,10 @@ public class VolumeDialogImpl implements VolumeDialog,
             }
         }
 
-        if (mVolumePanelOnLeft && isLandscape()) {
+        if (mVolumePanelOnLeft) {
             mTouchableRegion.op(
                     locInWindow[0],
-                    locInWindow[1],
+                    locInWindow[1] + (int) yExtraSize,
                     locInWindow[0] + view.getWidth() - (int) xExtraSize,
                     locInWindow[1] + view.getHeight(),
                     Region.Op.UNION);
@@ -1666,9 +1666,9 @@ public class VolumeDialogImpl implements VolumeDialog,
                 }, 50));
         if (!shouldSlideInVolumeTray()) {
             animator.translationX(getTranslationForPanelLocation() * mDialogView.getWidth() / 2.0f);
+            animator.setListener(getJankListener(getDialogView(), TYPE_DISMISS,
+                    mDialogHideAnimationDurationMs)).start();
         }
-        animator.setListener(getJankListener(getDialogView(), TYPE_DISMISS,
-                mDialogHideAnimationDurationMs)).start();
         checkODICaptionsTooltip(true);
         synchronized (mSafetyWarningLock) {
             if (mSafetyWarning != null) {
